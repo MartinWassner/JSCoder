b4560a9f92f5c81b6da95b5f779cc74971edb755
Martin Wassner <wassner.martin@gmail.com>
2022-09-21 23:58:44 -0300

    2da entrega final

--- main.js
+++ main.js
@@ -1,44 +1,128 @@
-let nombre = prompt("Ingrese el nombre del juego")
-let precio = parseFloat(prompt("Ingrese el precio del juego base"));
-let descuento = parseFloat(prompt("Ingrese el valor del descuento (ej: 10, 25, 50)"));
+//let nombre = "Terraria"
+//let precio = 1020//document.getElementById("precioBase");
+//let descuento = 5//document.getElementById("valorDesc");
 const valorImpuesto = 1.75;
-
-
-// Incorporación de arrays de objetos
-const juegos = []
-
-
-function juego(nombre, precio, descuento){
-   this.nombre = nombre;
-   this.precio = parseFloat(precio);
-   this.descuento = parseFloat(descuento); 
-}
 
 
 function valorJuegoFinal(num1,num2,num3){
    let res1 = num1 * ((100 - num2) / 100);
    let res2 = res1 * num3;
    
-   if(isNaN(num1) || isNaN(num2)){
-      
-      alert("No dejes campos vacíos");
-      
-   }else {
-      document.write ("<br>");
-      document.write ("El precio final de " + nombre + " es " + res2); //Precio final luego del descuento y el impuesto
-   }
+   return res2;
    
-   console.log (res1); //Precio base luego de aplicar el descuento
-}
-const juegoNuevo = new juego("Terraria", 200, 15);
-
-valorJuegoFinal(precio,descuento,valorImpuesto);
-
-function cargarJuegos(juego){
-   return juegos.push(juego);
 }
 
-cargarJuegos(juegoNuevo)
+// function Juego (nombre, precio, descuento){
+//    this.nombre = nombre;
+//    this.precio = precio;
+//    this.descuento = descuento;
+// }
+let boton0 = document.querySelector(".item0");
+let boton1 = document.querySelector(".item1");
+let boton2 = document.querySelector(".item2");
 
-console.log(juegos);
+function textoBoton(numero) {
+   numero.innerHTML = "Agregado";
+}
 
+
+
+// const juego1 = Juego("Terraria", 129, 25)
+// const juego2 = Juego("The Witcher 3", 96, 50)
+// const juego3 = Juego("Fallout 3", 315, 10)
+
+
+document.getElementById("precioFinal").innerHTML = valorJuegoFinal();
+
+let carro = document.querySelectorAll(".addCart");
+
+
+let juegos = [
+   {
+      nombre: "Terraria",
+      valor: 129,
+      descuento: 25,
+      inCart: 0
+   },
+   {
+      nombre: "The Witcher 3",
+      valor: 96,
+      descuento: 50,
+      inCart: 0
+   },
+   {
+      nombre: "Fallout 3",
+      valor: 315,
+      descuento: 10,
+      inCart: 0
+   }
+
+];
+
+for (let i=0; i < carro.length; i++){
+   carro[i].addEventListener("click", ()=>{
+      carroNumero(juegos[i]);
+      costoTotal(juegos[i])
+   })
+}
+
+function carroNumeroCargado() {
+   let juegoNumero = localStorage.getItem("carroNumero");
+
+   if(juegoNumero){
+      document.querySelector(".itemsCarrito span").textContent = juegoNumero;
+   }
+}
+
+function carroNumero(juegos){
+
+
+   let juegoNumero = localStorage.getItem("carroNumero");
+
+   juegoNumero = parseInt(juegoNumero);
+
+   if (juegoNumero){
+      localStorage.setItem("carroNumero", juegoNumero + 1)
+      document.querySelector(".itemsCarrito span").textContent = juegoNumero + 1;
+   } else {
+      localStorage.setItem("carroNumero", 1);
+      document.querySelector(".itemsCarrito span").textContent = 1;
+   }
+
+   agregarItems(juegos);
+
+}
+
+function agregarItems(juegos){
+   let carroItems = localStorage.getItem("juegosEnCarro");
+   carroItems = JSON.parse(carroItems);
+
+   if(carroItems != null) {
+      if (carroItems[juegos.nombre] == undefined){
+         carroItems = {
+            ...carroItems,
+            [juegos.nombre]: juegos
+         }
+      }
+      carroItems[juegos.nombre].inCart += 1 
+   } else{
+      juegos.inCart = 1;      
+      carroItems = {
+         [juegos.nombre]: juegos
+      }
+   }
+   localStorage.setItem("juegosEnCarro", JSON.stringify(carroItems))
+}
+
+function costoTotal(juegos) {
+   // console.log("voy a pagar", juegos.valor);
+   let valorCarro = localStorage.getItem("valorFinal");
+   valorCarro = parseInt(valorCarro)
+
+   console.log("el costo total es, ", valorCarro);
+
+   localStorage.setItem("valorFinal", (((juegos.valor * (100 - juegos.descuento)/100)*1.75)));
+
+}
+
+carroNumeroCargado();
